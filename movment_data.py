f0097a1c1277e49a55d8519ca4e1a4fb699c5b35
Tuss4 <tuss4dbfn@gmail.com>
2014-05-11 23:38:05 -0500

    Properly linted using flake8

--- movment_data.py
+++ movment_data.py
@@ -6,17 +6,19 @@ kicks = ['540', 'hook', 'tornado', 'cheat 720']
 flips = ['front tuck', 'back tuck', 'side flip']
 twists = ['btwist', 'cork', 'full twist']
 
+
 def combo_generator():
-	'''The combo generator function'''
-	# setup your random tricks
-	k = random.choice(kicks)
-	f = random.choice(flips)
-	t = random.choice(twists)
-	return [k, t, f]
+    '''The combo generator function'''
+    # setup your random tricks
+    k = random.choice(kicks)
+    f = random.choice(flips)
+    t = random.choice(twists)
+    return [k, t, f]
+
 
 def multi_combos():
-	'''List of combos'''
-	for i in range(10):
-		print combo_generator()
+    '''List of combos'''
+    for i in range(10):
+        print combo_generator()
 
 multi_combos()
